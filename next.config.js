41ef64e2d2021d9aabb1a071429be0a090e4e28c
Nachiketh Kenchem <123257042+NachikethKenchem@users.noreply.github.com>
2025-02-01 08:47:27 +0300

    feat(movie-grid): Add shimmer effect for loading states and update image optimization settings

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   images: {
     domains: ['image.tmdb.org'],
-    unoptimized: true
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 640, 750, 828, 1080],
   },
   env: {
     NEXT_PUBLIC_TMDB_API_URL: process.env.NEXT_PUBLIC_TMDB_API_URL,
